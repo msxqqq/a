33841db85fdd4dc42dfd6bdba24fdfae051d6e4f
msxqqq <43518691+msxqqq@users.noreply.github.com>
2021-06-22 13:26:01 +0800

    Delete py directory

--- py/fdfd.py
+++
@@ -1,7 +0,0 @@
-a = ''
-b = input('输入字符串')
-n = len(b)
-for c in range(n):
-    print(c)
-    a = a +b[n-1-c]
-print(a)
--- py/lala.py
+++
@@ -1,6 +0,0 @@
-class zhu():
-    a = '猪'
-    def b(self,jiji):
-        print('你好啊{}{}'.format(jiji,self.a))
-haha = zhu()
-haha.b('小')
